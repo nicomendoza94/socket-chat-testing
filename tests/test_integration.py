da5b789699663f2eb4b3a9be3f3d3bdf9d68420f
Nicolas Mendoza <nicolasmenndoza@gmail.com>
2026-07-01 16:54:12 -0300

    test: add chat integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -62,3 +62,102 @@ def test_server_broadcasts_messages_between_clients():
 
     sender_client.close()
     receiver_client.close()
+
+def test_multiple_clients_receive_same_message():
+    # Todos los clientes conectados, excepto el emisor, deben recibir el mensaje.
+
+    sender_client = create_client()
+    receiver_client_1 = create_client()
+    receiver_client_2 = create_client()
+
+    time.sleep(0.2)
+
+    message = "Mensaje para todos"
+
+    sender_client.send(message.encode("utf-8"))
+
+    received_message_1 = receiver_client_1.recv(1024).decode("utf-8")
+    received_message_2 = receiver_client_2.recv(1024).decode("utf-8")
+
+    assert message in received_message_1
+    assert message in received_message_2
+
+    sender_client.close()
+    receiver_client_1.close()
+    receiver_client_2.close()
+
+
+#los mensajes deben llegar en el mismo orden en que fueron enviados
+def test_messages_are_received_in_order():
+    sender_client = create_client()
+    receiver_client = create_client()
+
+    time.sleep(0.2)
+
+    messages = ["Uno", "Dos", "Tres"]
+
+    for message in messages:
+        #pausa breve para facilitar que el servidor procese cada envio
+        sender_client.send(message.encode("utf-8"))
+        time.sleep(0.1)
+
+    received_data = ""
+
+    while not all(message in received_data for message in messages):
+        received_data += receiver_client.recv(1024).decode("utf-8")
+
+    assert received_data.index("Uno") < received_data.index("Dos")
+    assert received_data.index("Dos") < received_data.index("Tres")
+
+    sender_client.close()
+    receiver_client.close()
+
+
+#el servidor debe continuar funcionando aunque un cliente se desconecte inesperadamente
+def test_client_disconnection_does_not_stop_server():
+
+    disconnected_client = create_client()
+    active_client = create_client()
+    receiver_client = create_client()
+
+    time.sleep(0.2)
+
+    disconnected_client.close()
+
+    time.sleep(0.2)
+
+    message = "Servidor sigue activo"
+
+    active_client.send(message.encode("utf-8"))
+
+    received_message = receiver_client.recv(1024).decode("utf-8")
+
+    assert message in received_message
+
+    active_client.close()
+    receiver_client.close()
+
+
+#el receptor debe recibir una unica copia del mensaje enviado
+def test_message_is_not_duplicated():
+
+    sender_client = create_client()
+    receiver_client = create_client()
+
+    time.sleep(0.2)
+
+    message = "Mensaje unico"
+
+    sender_client.send(message.encode("utf-8"))
+
+    first_message = receiver_client.recv(1024).decode("utf-8")
+
+    receiver_client.settimeout(0.5)
+
+    with pytest.raises(socket.timeout):
+        receiver_client.recv(1024)
+
+    assert message in first_message
+
+    sender_client.close()
+    receiver_client.close()
